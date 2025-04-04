a7c2cc547816caf375a50cf4717049823a7cc7e7
msmch <m.smiechowicz@hotmail.com>
2025-04-04 21:42:21 +0200

    Update of integrations and the core script

--- warehouse/integrations/azure_notification_integration.sql
+++ warehouse/integrations/azure_notification_integration.sql
@@ -5,7 +5,7 @@ create notification integration azure_notification
 enabled = true
 type = queue
 notification_provider = azure_storage_queue
-azure_storage_queue_primary_uri = 'https://msmch.queue.core.windows.net/snowflake'
+azure_storage_queue_primary_uri = 'azure://msmchsnowflakestater.blob.core.windows.net/football-data'
 azure_tenant_id = 'TENANT_ID_GOES_HERE'; -- you need to add your TENANT_ID manually before running the script
 
 
--- warehouse/integrations/azure_storage_integration.sql
+++ warehouse/integrations/azure_storage_integration.sql
@@ -1,6 +1,6 @@
 /* 
 The storage integration allowing snowflake to access data that is stored in the Azure Cloud. All blobs are allowed 
-in my dedicated container snowflake-starter. This way we give Snowflake control on one container only and it's not 
+in my dedicated container football-data. This way we give Snowflake control on one container only and it's not 
 able to interact with other objects in the storage account.
 */
 use role accountadmin;
@@ -11,7 +11,7 @@ type = external_stage
 storage_provider = azure
 enabled = true
 azure_tenant_id = 'TENANT_ID_GOES_HERE' -- make sure to copy paste the tenant ID 
-storage_allowed_locations = ('azure://msmch.blob.core.windows.net/snowflake-starter');
+storage_allowed_locations = ('azure://msmchsnowflakestater.blob.core.windows.net/football-data');
 
 -- then run the below and open AZURE_CONSENT_URL to authorize connection
 desc storage integration azure_integration;
